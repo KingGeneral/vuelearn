68a69fed70b76f0ffd6d05e10d4bae0f0875d0fa
zonetm <zone.nano09@gmail.com>
2019-02-10 17:40:23 +0700

    test 2

--- test2.php
+++ test2.php
@@ -9,16 +9,29 @@
 	  <button onclick="vm.increment()"> + </button>
 	  <p>{{ fullName }}</p>
 	  <p>{{ timeNow }}</p>
+	  <p>{{ fullName | upper }}</p> <!-- filter -->
+	  <p>{{ message | upper }}</p> <!-- filter -->
+	  <p>{{ message | upperouter }}</p> <!-- filter -->
+	  <p>{{ message | upper | reverse }}</p>
+	  <p> {{ price | formatCurrency('USD') }} </p>
+		<p> {{ price | formatCurrency('IDR') }} </p>
   </div>
 
   <!-- start script -->
   <script type="text/javascript">
+
+  	Vue.filter('upperouter', function (value) {
+		  return value.toUpperCase()
+		});
+
   	var vm = new Vue({
   		el:'#app',
   		data:{
   			counter: 0,
   			firstName: 'Anthony',
-  			lastName: 'Kal'
+  			lastName: 'Kal',
+  			message: 'Hello World',
+  			price: 500000
   		},
   		// Properti methods dapat berisi fungsi-fungsi (Javascript tentunya) yang dapat dipanggil disemua tempat pada aplikasi berbasis Vue. 
   		methods:{
@@ -34,8 +47,34 @@
   			timeNow: function() {
   				return Date.now();
   			}
+  		},
+  		// properti filters yang dapat berisi fungsi untuk digunakan memanipulasi tampilan atau format 
+  		filters: {
+  			upper(text) {
+  				return text.toUpperCase();
+  			},
+  			reverse(text) {
+  				return text.split('').reverse().join('');
+  			},
+  			formatCurrency(value, currency) {
+  				var formatter = new Intl.NumberFormat('id-ID',{
+  					style: 'currency',
+  					currency: currency,
+  					minimumFractionDigits: 2.
+  				});
+					return formatter.format(value);  				
+  			}
   		}
   	});
+
+  	/*
+		<!-- filters -->
+			<h1>Filters: {{ message | upper }}</h1>
+		<!-- methods -->
+			<h1>Methods: {{ upper(message) }}</h1>
+		<!-- computed -->
+			<h1>Computed: {{ messageUpperCase }}</h1>
+  	*/
 
   	// auto interval
   	setInterval( () => {

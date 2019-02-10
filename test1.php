3f091addfb038e05cd90b9f7d575234387a991af
zonetm <zone.nano09@gmail.com>
2019-02-10 17:05:09 +0700

    final test 1

--- test1.php
+++ test1.php
@@ -6,22 +6,46 @@
 </head>
 <body>
   <div id="app">
-    <h1>{{ message }}</h1>
+    <h1 v-once>called once - {{ message }}</h1> <!-- called once -->
+    <h1>can be change forever - {{ message }}</h1>
+    <h1>{{ url_message }}</h1>
+    <h1 v-html="url_message"></h1> <!-- open html data -->
     <h1>{{ 'Pesan - ' + message }}</h1>
     <p>{{ teks }}</p>
+    <hr>
+    <h1 v-bind:class="class_h1">{{ message_h1 }}</h1> <!-- class dynamic -->
+    <hr>
+    <p>{{ 'Total Price : ' + total_price }}</p>
+    <p>{{ 'Diskon : ' + (total_price * 0.1) }}</p>
+    <p>{{ 'Gender : ' + (gender ? 'male' : 'female') }}</p>
+    <p>{{ (gender) ? 'yes' : 'no'}}</p>
+    <p><a v-bind:href="'product-' + index">data</a></p>
+    {{ a = 1 }}
+    <p>{{ a }}</p>
   </div>
+  <!-- app2 -->
+  <hr><hr>
+  <hr><hr>
+  <hr><hr>
+  <div id="app2"></div>
 
   <!-- start script -->
   <script type="text/javascript">
+  // app
 	  var vm = new Vue({
 	  	el: '#app',
 	  	data:{
 	  		message : 'Hello, Ini VUE',
+	  		url_message : "<span style='color:red'>Hello World!</span>",
 	  		teks 	: "selamat pagi indonesia",
 	  		age 	: 33, // int
 	  		gender 	: true, // male
 	  		hobby 	: ['coding','sleep'], // array
-	  		children: {1:'Amm',2:'Faq',3:'Sya'} //object 
+	  		children: {1:'Amm',2:'Faq',3:'Sya'}, //object 
+	  		message_h1: 'hello with dynamic class',
+	  		class_h1: 'title',
+	  		total_price:10000,
+	  		index: 1
 	  	},
 	  	beforeCreate() {
 	  		// beforeCreate yaitu hook sesaat setelah objek Vue dan komponennya diinisialisasi. Properti data belum dapat diakses atau digunakan pada hook ini.
@@ -59,7 +83,7 @@
 
 	  // init
 	  // untuk mengarahkan mount point Vue pada saat runtime.
-	  vm.$mount('#app');
+	  // vm.$mount('#app');
 
 	  // test watcher
 	  vm.$watch('message',function(newValue, oldValue) {
@@ -74,6 +98,22 @@
 	  // console.log(vm.$data);
 	  // console.log(vm.$data.message);
 	  // console.log(vm.message);
+
+  </script>
+  <script type="text/javascript">
+	  // app2
+	  var vm2 = new Vue({
+	  	// el: '#app2',
+	  	data:{
+	  		message: 'hohoho ini app2',
+	  	},
+	  	// jika properti template dan render dua-duanya ada maka properti template akan diabaikan.
+	  	// template: "<h1>{{ message }}</h1>"
+	  	render(createElement) {
+	  		return createElement('h1', this.message);
+	  	}
+	  })
+	  vm2.$mount('#app2');
 
   </script>
   <!-- end script -->

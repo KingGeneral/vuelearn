22a9957491e0bb8e79ce1164a9d2fe2a33c2552a
KingGeneral <9468485+KingGeneral@users.noreply.github.com>
2019-02-12 16:23:51 +0700

    more test4 array list

--- test4.php
+++ test4.php
@@ -35,6 +35,29 @@
   			{{ index + 1 }}, {{ book }}
   		</li>
   	</ul>
+
+    <hr>
+
+    <!-- array in value -->
+    <!-- array in value -->
+
+    <ul>
+      <li v-for="value of books2">{{value}}</li>
+    </ul>
+    
+    <ul>
+      <li v-for="(value, key) of books2">
+        {{ key }} : {{ value }}
+      </li>  
+    </ul>
+    
+    <ul>
+      <li v-for="(value,key,index) of books2">
+        {{ index+1 }}, {{key}} : {{value}}
+      </li>  
+    </ul>
+
+    <!-- end of app -->
   </div>
 
   <script type="text/javascript">
@@ -43,10 +66,18 @@
   		data: {
   			books: [
   				'C++ High Performance', 
-            	'Mastering Linux Security and Hardening', 
-            	'Python Programming Blueprints', 
-            	'Mastering PostgreSQL 10'
-            ]
+        	'Mastering Linux Security and Hardening', 
+        	'Python Programming Blueprints', 
+        	'Mastering PostgreSQL 10'
+        ],
+        books2:{
+          id            : 99,
+          title         : 'C++ High Performance',
+          description   : 'Write code that scales across CPU registers, multi-core, and machine clusters',
+          authors       : 'ztm',
+          publish_year  : 2018,
+          price         : 150000
+        }
   		}
   	});
   </script>

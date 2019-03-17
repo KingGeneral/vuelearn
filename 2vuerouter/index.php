5c2e1d18bc23bb7a4313e9d4117fc4abeeb5187f
Anthony Kal <9468485+KingGeneral@users.noreply.github.com>
2019-03-17 20:59:48 +0700

    add router

--- 2vuerouter/index.php
+++ 2vuerouter/index.php
@@ -8,8 +8,43 @@
 	  	</style>
 	</head>
 	<body>
-		<div id="app"></div>
+		<div id="app">
+			<p>
+				<router-link to="/">Home</router-link>
+				<router-link to="/about">About</router-link>
+			</p>
+			<router-view></router-view>
+		</div>
 	</body>
 	<script type="text/javascript">
+		// definisikan konfigurasi component
+		const Home = { template: '<div>Halaman Home</div>'}
+		const About = { template: '<div>Halaman About</div>'}
+
+		// mapping route path dengan componentnya
+		const routes = [
+			{ path: '/', component: Home, alias: '/home'},
+			{ path: '/about', component: About},
+			{ path: '*', redirect: '/' }
+		]
+
+		// register routes pada objek router
+		const router = new VueRouter({
+			// routes:routes // version long
+			routes // version short 
+		})
+
+		// register objek router pada objek Vue
+		var vm = new Vue({
+			el: '#app',
+			router,
+		})
+
+		/*
+		Dengan menggunakan Vue Router ini maka seluruh routing yang pernah kita klik akan tersimpan di-history browser sehingga ketika user mengklik button back, URL akan sesuai dengan routing yang terakhir kali diakses.
+
+		Catatan: current route bisa kita akses dalam objek Vue dengan perintah this.$route.path atau pada template dengan perintah {{ $route.path }}.
+		*/
+
 	</script>
 </html>

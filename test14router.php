eb92d9194c4f486e1e97744804e2daf18f931244
Anthony Kal <9468485+KingGeneral@users.noreply.github.com>
2019-03-17 20:36:55 +0700

    add vue, vue router, test router

---
+++ test14router.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  	<title>Belajar Vue</title>
+	  	<script src="lib/vue.js"></script>
+	  	<script src="lib/vue-router.js"></script>
+	  	<style type="text/css">
+	  	</style>
+	</head>
+	<body>
+		<div id="app"></div>
+	</body>
+	<script type="text/javascript">
+	</script>
+</html>
